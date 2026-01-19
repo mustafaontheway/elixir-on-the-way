22a15c8a805d34b9831fee7ae9fa417bf8d165e5
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-19 19:31:21 +0300

    Create part-z-0003-basic-math2.ex

---
+++ part-z-0003-basic-math2.ex
@@ -0,0 +1,21 @@
+IO.inspect rem(11, 7), label: "Remainder"
+
+IO.inspect round(7.52), label: "Round 1"
+
+IO.inspect round(7.49), label: "Round 2"
+
+IO.inspect ceil(7.12), label: "Ceil 1"
+
+IO.inspect ceil(7.54), label: "Ceil 2"
+
+IO.inspect trunc(7.12), label: "Trunc 1"
+
+IO.inspect trunc(7.54), label: "Trunc 2"
+
+# Remainder: 4
+# Round 1: 8
+# Round 2: 7
+# Ceil 1: 8
+# Ceil 2: 8
+# Trunc 1: 7
+# Trunc 2: 7
