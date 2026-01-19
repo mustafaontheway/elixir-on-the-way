d049d424d8bad3fe60a1cbd666ecb8060b0eb3b0
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-19 11:22:53 +0300

    Create part-z-0002-inspect3.ex

---
+++ part-z-0002-inspect3.ex
@@ -0,0 +1,7 @@
+# IO.inspect prints a representation of any value
+
+# We can specify a 'label' to include additional info
+
+# We can also omit braces
+
+IO.inspect 32 + 87, label: "x + y" # x + y: 119
