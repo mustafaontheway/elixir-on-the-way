70ad6aa2b2130cbc1cd7abd6aeefa711277446ed
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-19 19:24:39 +0300

    Create part-z-0003-basic-math1.ex

---
+++ part-z-0003-basic-math1.ex
@@ -0,0 +1,12 @@
+IO.inspect 10 + 24, label: "Sum: "
+
+IO.inspect 10 - 24, label: "Diff: "
+
+IO.inspect 10 * 24, label: "Mult: "
+
+IO.inspect 24 /10 , label: "Div: "
+
+# Sum: : 34
+# Diff: : -14
+# Mult: : 240
+# Div: : 2.4
