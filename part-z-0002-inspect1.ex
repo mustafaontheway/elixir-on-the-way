bedd36fae1594127154a31544133fbdd2d16b652
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-19 11:19:03 +0300

    Create part-z-0002-inspect1.ex

---
+++ part-z-0002-inspect1.ex
@@ -0,0 +1,15 @@
+# IO.inspect prints a representation of any value
+
+IO.inspect(32 + 87)
+
+IO.inspect(12 * -54)
+
+IO.inspect(12 - 54)
+
+IO.inspect(12 / -54)
+
+# Output:
+# 119
+# -648
+# -42
+# -0.2222222222
