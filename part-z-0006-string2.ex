e2a3b0d1e44b373ba379ccbd880714bba874c069
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-20 11:40:45 +0300

    Create part-z-0006-string2.ex

---
+++ part-z-0006-string2.ex
@@ -0,0 +1,9 @@
+# concatenate strings with <> operator
+
+fullname = "Mustafa Büyükdereli"
+
+year = 2026
+
+IO.inspect "Yes, #{fullname}" <> " " <> "is here in #{year}."
+
+# "Yes, Mustafa Büyükdereli is here in 2026."
